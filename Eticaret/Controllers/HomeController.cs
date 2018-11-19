62d4a2f64af4bfa46bd97093888752d95fa0c1ff
tunaysabriyilmaz <tunaysabriyilmaz@medicawell.com>
2018-11-19 09:53:52 +0300

    proje repo oluşturuldu

---
+++ Eticaret/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eticaret.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+
+        
+    }
+}
